c18ea05484719dad2133c16d57f0ea07db711c42
Mike North <michael.l.north@gmail.com>
2017-01-11 13:46:55 -0500

    first exercise

---
+++ app/routes/application.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+const { Route } = Ember;
+
+export default Route.extend({
+  model() {
+    return [{
+      title: 'Post Title 1',
+      body: `## This is my body
+And some more stuff
+`,
+      authorName: 'Somebody User',
+      authorPhotoUrl: 'http://placekitten.com/g/101/100'
+    }, {
+      title: 'Post Title 2',
+      body: `## This is my body
+And some more stuff
+`,
+      authorName: 'Somebody User',
+      authorPhotoUrl: 'http://placekitten.com/g/101/100'
+    }, {
+      title: 'Post Title 3',
+      body: 'This is my body',
+      authorName: 'Somebody User',
+      authorPhotoUrl: 'http://placekitten.com/g/101/100'
+    }];
+  }
+});
