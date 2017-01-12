f4e771e514597ad6e5f425fb377355bdda371820
Mike North <michael.l.north@gmail.com>
2017-01-12 13:19:42 -0500

    Fastboot

--- app/initializers/get-location.js
+++ app/initializers/get-location.js
@@ -1,20 +1,23 @@
 export function initialize(app) {
-  let geo = navigator.geolocation;
-  app.deferReadiness();
-  geo.getCurrentPosition((pos) => {
-    let pt = pos.coords;
-    let loc = {
-      lat: pt.latitude,
-      lng: pt.longitude
-    };
-    app.register('data:location', loc, {
-      singleton: true,
-      instantiate: false
-    });
-    // app.inject('controller', 'location', 'data:location');
 
-    app.advanceReadiness();
-  });
+  if (typeof FastBoot === 'undefined') {
+    let geo = navigator.geolocation;
+    app.deferReadiness();
+    geo.getCurrentPosition((pos) => {
+      let pt = pos.coords;
+      let loc = {
+        lat: pt.latitude,
+        lng: pt.longitude
+      };
+      app.register('data:location', loc, {
+        singleton: true,
+        instantiate: false
+      });
+      // app.inject('controller', 'location', 'data:location');
+
+      app.advanceReadiness();
+    });
+  }
 }
 
 export default {
