ec764e8ad4536593530aa5eb1d70fc326fab0b0a
Mike North <michael.l.north@gmail.com>
2017-01-12 12:47:27 -0500

    initializers

---
+++ app/controllers/posts/index.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+const { Controller, computed, getOwner } = Ember;
+
+export default Controller.extend({
+  loc: computed(function() {
+    return getOwner(this).lookup('data:location');
+  })
+});
---
+++ app/initializers/get-location.js
@@ -0,0 +1,23 @@
+export function initialize(app) {
+  let geo = navigator.geolocation;
+  app.deferReadiness();
+  geo.getCurrentPosition((pos) => {
+    let pt = pos.coords;
+    let loc = {
+      lat: pt.latitude,
+      lng: pt.longitude
+    };
+    app.register('data:location', loc, {
+      singleton: true,
+      instantiate: false
+    });
+    // app.inject('controller', 'location', 'data:location');
+
+    app.advanceReadiness();
+  });
+}
+
+export default {
+  name: 'get-location',
+  initialize
+};
