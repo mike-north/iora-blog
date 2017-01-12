91f32da084c687df111f123cec14a5cad54ef570
Mike North <michael.l.north@gmail.com>
2017-01-12 16:34:18 -0500

    state-services: draft state

--- app/components/post-full.js
+++ app/components/post-full.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
+import stateFor from 'ember-state-services/state-for';
 
 const { Component } = Ember;
 
 export default Component.extend({
   classNames: ['post-full'],
-  attributeBindings: ['model.id:post-id']
+  attributeBindings: ['model.id:post-id'],
+  draftComment: stateFor('post-info', 'model')
 });
---
+++ app/states/post-info.js
@@ -0,0 +1,11 @@
+/* eslint ember-suave/no-direct-property-access:"off" */
+import Ember from 'ember';
+
+export default Ember.Object.extend();
+
+
+// // reopenClass
+// MyStateObject.initialState;
+
+// // reopen
+// MyStateObject.create().initialState();
