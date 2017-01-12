7fdbf2bcd9bc536794f1d47dbba26e00560bae79
Mike North <michael.l.north@gmail.com>
2017-01-12 11:11:37 -0500

    ES6-ify Math.*

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,4 +1,4 @@
-/*jshint node:true*/
+/* eslint-env node*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
@@ -6,6 +6,8 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
+
+  app.import('./vendor/math.js');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
